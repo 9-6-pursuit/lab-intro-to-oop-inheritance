8004dfa2f05f5d09226496c68cefec81165843b6
Joanavel Pascual <joanavelpascual@pursuit.org>
2023-09-24 12:48:06 -0400

    food passing

--- tamagotchi.js
+++ tamagotchi.js
@@ -1,4 +1,17 @@
 // Create class below
 
+class Tamagotchi {
+    constructor (name) {
+        this.name = name
+        this.energy = 9
+        this.full = 8
+        this.mood = 6
+        this.sick = false
+        this.rehomed = false
+    }
+}
+
 // Do not edit below this line
 module.exports = Tamagotchi;
+
+
