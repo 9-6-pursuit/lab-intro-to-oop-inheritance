e0f928ab872f8e8c5c2a27b7613f580d4a74a7cc
Ahmed Heggy <ahmedheggy@pursuit.org>
2023-09-24 12:43:07 -0400

    created Food class

--- food.js
+++ food.js
@@ -1,4 +1,30 @@
 // Create class below
+class Food {
+  constructor(name, daysToSpoil, fresh) {
+    this.name = name;
+    this.daysToSpoil = daysToSpoil;
+    this.fresh = fresh;
+  }
+
+  prepare() {
+    console.log(`the ${this.name} is being prepared`);
+  }
+
+  isFresh() {
+    this.daysToSpoil
+      ? `There are ${this.daysToSpoil} days left before ${this.name} spoils`
+      : `${this.name} has spoiled.`;
+
+      if(this.daysToSpoil === 0){
+        this.fresh = false
+      }
+  }
+
+  aDayPasses() {
+    this.daysToSpoil--;
+    this.isFresh
+  }
+}
 
 // Do not edit below this line
 module.exports = Food;
--- tamagotchi.js
+++ tamagotchi.js
@@ -1,4 +1,22 @@
 // Create class below
 
+class Tamagotchi {
+    constructor(name){
+        this.name = name
+        this.energy = 9
+        this.full = 8
+        this.mood = 6
+        this.sick = false
+        this.rehome = false
+    }
+
+    greet(){
+
+    }
+
+    status(){
+        
+    }
+}
 // Do not edit below this line
 module.exports = Tamagotchi;
