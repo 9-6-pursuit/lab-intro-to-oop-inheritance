e1029b7c4724ba8d5756148e7a6f180cff581130
Monir Hossen <monirhossen@pursuit.org>
2023-09-26 20:44:11 -0400

    All tests passed in Food class

--- food.js
+++ food.js
@@ -1,4 +1,30 @@
 // Create class below
 
+class Food{
+    constructor (name,daysToSpoil = 0 ){
+        this.name = name
+        this.daysToSpoil = daysToSpoil
+        this.fresh = true
+    }
+
+    prepare () {
+        console.log(`${this.name} is being prepared`)
+    }
+
+    isFresh (){
+        console.log(
+            this.daysToSpoil > 0 ?
+            `There are ${this.daysToSpoil} days left before ${this.name} spoils.` : 
+            `${this.name} has spoiled.`
+        )
+    }
+
+    aDayPasses() {
+        this.daysToSpoil--
+        this.isFresh()
+    }
+}
+
+
 // Do not edit below this line
 module.exports = Food;
--- tamagotchi.js
+++ tamagotchi.js
@@ -1,4 +1,8 @@
 // Create class below
+class Tamagotchi {
+
+
+}
 
 // Do not edit below this line
 module.exports = Tamagotchi;
