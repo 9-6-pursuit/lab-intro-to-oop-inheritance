f9ca47c46640617d092944816bd650811b53861f
Frantz-Sebastien Mathias <frantz-sebastienmathias@pursuit.org>
2023-09-27 03:37:04 -0400

    finishing lab

--- food.js
+++ food.js
@@ -1,4 +1,33 @@
 // Create class below
 
+class Food {
+    constructor(name, daysToSpoil){
+        this.name = name;
+        this.daysToSpoil = daysToSpoil;
+        this.fresh = true
+    }
+    // OR this BELOW
+    // constructor(name, daysToSpoil, fresh = true){
+    //     this.name = name;
+    //     this.daysToSpoil = daysToSpoil;
+    //     this.fresh = fresh
+    // }
+
+    prepare(){
+        console.log(`${this.name} is being prepared`)
+    }
+
+    isFresh(){
+        console.log(
+            this.daysToSpoil > 0 ? `There are ${this.daysToSpoil} days left before ${this.name} spoils.` : `${this.name} has spoiled.`
+        )
+    }
+
+    aDayPasses(){
+        this.daysToSpoil--
+        this.isFresh()
+    }
+}''
+
 // Do not edit below this line
 module.exports = Food;
--- tamagotchi.js
+++ tamagotchi.js
@@ -1,4 +1,26 @@
 // Create class below
+class Tamagotchi {
+    constructor(name, energy = 9 , full = 8, mood = 6, sick = false, rehomed = false){
+        this.name = name;
+        this.energy = energy;
+        this.full = full;
+        this.mood = mood;
+        this.sick = sick;
+        this.rehomed = rehomed;
+    }
 
+    greet(){
+        console.log(`Hello, I'm ${this.name}`)
+    }
+
+    status(){
+        console.log(`********************
+        My mood is: ${this.mood}
+        I am this full:
+        My energy is
+        I am not sick
+        ********************`)
+    }
+}
 // Do not edit below this line
 module.exports = Tamagotchi;
