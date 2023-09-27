2f4899c0fa4d9d053699b42d6ee5f31374a33e56
JenniferEinstein <jennifereinstein@gmail.com>
2023-09-26 22:02:07 -0400

    First commit now that my laptop is connected to the internet.

--- food.js
+++ food.js
@@ -1,4 +1,28 @@
 // Create class below
 
+class Food {
+    constructor(name, daysToSpoil, fresh=true){
+        this.name=name;
+        this.daysToSpoil=daysToSpoil;
+        this.fresh=fresh
+    }
+
+    prepare(name){
+        console.log(`${name} is being prepared`)
+    }
+
+    isFresh(daysToSpoil, name){
+        if(daysToSpoil<1){
+            console.log(`${name} has spoiled.`)
+        } else {
+        console.log(`There are ${daysToSpoil} days left before ${name} spoils.`)
+        }
+    }
+
+    aDayPasses(){
+
+    }
+}
+
 // Do not edit below this line
 module.exports = Food;
