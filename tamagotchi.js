c7c4c1a65c1560470ed98e08402843c908675d95
Monir Hossen <monirhossen@pursuit.org>
2023-09-27 16:24:52 -0400

    1/4 of Tamagotchi tests has passed

--- tamagotchi.js
+++ tamagotchi.js
@@ -1,6 +1,17 @@
 // Create class below
 class Tamagotchi {
+    constructor(name){
+        this.name = name
+        this.energy = 9
+        this.full = 8
+        this.mood = 6
+        this.sick = false
+        this.rehomed = false
+    }
 
+    greet (){
+        console.log(`Hello, I'm ${this.name}!`)
+    }
 
 }
 
