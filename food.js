347170e2d1d60a00383e55e02ad738e47efb286f
Lakisha J <lakishajohnson@pursuit.org>
2023-10-01 14:35:26 -0400

    Added methods: prepare, isFresh, and aDayPasses

--- food.js
+++ food.js
@@ -5,6 +5,24 @@ class Food {
     this.daysToSpoil = daysToSpoil;
     this.fresh = true;
   }
+
+  prepare() {
+    console.log(`${this.name} is being prepared`);
+  }
+
+  isFresh() {
+    if (this.daysToSpoil >= "0") {
+      console.log(
+        `There are ${this.daysToSpoil} days left before ${this.name} spoils.`
+      );
+    } else {
+      console.log(`${this.name} has spoiled.`);
+    }
+  }
+  aDayPasses() {
+    this.daysToSpoil = this.daysToSpoil - 1;
+    this.isFresh();
+  }
 }
 
 // Do not edit below this line
