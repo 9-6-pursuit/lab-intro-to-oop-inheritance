f9c2d382f7cd6a195b361c49fd2faa946bc7b66e
Lakisha J <lakishajohnson@pursuit.org>
2023-10-01 16:14:26 -0400

    Added methods: greet, status, eat, medicate, play and sleep

--- tamagotchi.js
+++ tamagotchi.js
@@ -15,6 +15,60 @@ class Tamagotchi {
     this.sick = sick;
     this.rehomed = rehomed;
   }
+
+  greet() {
+    console.log(`Hello, I'm ${this.name}!`);
+  }
+
+  status() {
+    console.log(
+      `My energy is: ${this.energy}`,
+      `I am this full: ${this.full}`,
+      `My mood is: ${this.mood}`,
+      "I am not sick"
+    );
+  }
+
+  eat() {
+    this.full = this.full + 2;
+    this.energy = this.energy - 1;
+    if (this.full > 10) {
+      this.sick = true;
+    }
+  }
+
+  medicate() {
+    if (this.sick === true) {
+      this.full = 9;
+      this.energy -= 3;
+      this.sick = false;
+    } else {
+      console.log("Refuses to take medicine");
+      this.energy -= 1;
+    }
+  }
+
+  play() {
+    if (this.sick === true) {
+      this.mood -= 1;
+      this.energy -= 1;
+    } else if (this.mood > 9) {
+      this.energy -= 2;
+      this.full -= 1;
+    } else if (this.energy <= 3) {
+      console.log("I am too tired to play");
+      this.energy -= 1;
+    } else {
+      this.mood += 2;
+      this.energy -= 1;
+      this.full -= 1;
+    }
+  }
+
+  sleep() {
+    this.energy += 4;
+    this.full -= 3;
+  }
 }
 
 // Do not edit below this line
