96e01c6cab644c9eb39927c76afee0b606a7921c
Lakisha J <lakishajohnson@pursuit.org>
2023-10-01 14:33:37 -0400

    Created a food class using constructor

--- food.js
+++ food.js
@@ -1,4 +1,11 @@
 // Create class below
+class Food {
+  constructor(name, daysToSpoil, fresh) {
+    this.name = name;
+    this.daysToSpoil = daysToSpoil;
+    this.fresh = true;
+  }
+}
 
 // Do not edit below this line
 module.exports = Food;
