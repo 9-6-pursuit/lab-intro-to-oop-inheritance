b0f0868db4f1285da50ce041559310b2a411d299
JenniferEinstein <jennifereinstein@gmail.com>
2023-10-04 11:32:16 -0400

    Add constructor and method outline to Tamagotchi class.

--- tamagotchi.js
+++ tamagotchi.js
@@ -1,9 +1,44 @@
 // Create class below
 class Tamagotchi {
-    constructor(name, daysToSpoil, fresh=true){
+    constructor(name, energy=9, full=8, mood=6, sick=false, rehomed=false){
         this.name=name;
-        this.daysToSpoil=daysToSpoil;
-        this.fresh=fresh
+        this.energy=energy;
+        this.full=full;
+        this.mood=mood;
+        this.sick=sick;
+        this.rehomed=rehomed;
+    }
+
+    greet(name){
+        `Hello, I'm ${name}!`
+    }
+
+    status(energy, full, mood, sick){
+        console.log(`********/nMy mood is: ${mood} /nI am this full: ${full} /nMy energy is: ${energy} /n "I am sick" : "I am not sick"/n*******`)
+    }
+
+    eat(){
+
+    }
+
+    medicate(){
+
+    }
+
+    play(){
+        this.mood=
+    }
+
+    sleep(){
+
+    }
+
+    timePasses(){
+
+    }
+
+    badGuardian(){
+
     }
 }
 // Do not edit below this line
